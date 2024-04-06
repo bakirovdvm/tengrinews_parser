58bca4e4b7fb9fa1574a2752fc5dda6feaee72fb
Сергали <bakirovdvm@gmail.com>
2024-04-06 15:38:24 +0600

    пробуем вытащить заголовок, текст и главную картинку новости

--- main.py
+++ main.py
@@ -26,26 +26,40 @@ soup = BeautifulSoup(page.text, "html.parser")
 # pagination_count = int(soup.find('ul', class_='pagination').find_all('a')[-2].text)
 # print(pagination_count)
 
+#
+# articles_urls_list = list()
+#
+# pagination_count = 10
+# for page in range(1, pagination_count+1):
+#
+#     response = requests.get(url=f'https://tengrinews.kz/news/page/{page}/', headers=headers)
+#     soup = BeautifulSoup(response.text, "html.parser")
+#
+#     articles_urls = soup.find_all(class_='content_main_item_title')
+#     for au in articles_urls:
+#         art_url = au.find('a').get('href')
+#         articles_urls_list.append(f'https://tengrinews.kz{art_url}')
+#
+#     time.sleep(randrange(2, 4))
+#
+#     with open('articles_urls_list.txt', 'w') as file:
+#         for url in articles_urls_list:
+#             file.write(f'{url}\n')
+#
+# print(f'Операция записи ссылок в файл завершена!\n'
+#       f'Проанализировано {pagination_count} страниц')
 
-articles_urls_list = list()
+s_url = 'https://tengrinews.kz/kazakhstan_news/tokaev-pavodkah-vozmojno-samoe-krupnoe-bedstvie-poslednie-80-531444/'
+s_page = requests.get(s_url)
+s_soup = BeautifulSoup(s_page.text, "html.parser")
 
-pagination_count = 10
-for page in range(1, pagination_count+1):
+news_title = s_soup.find(class_='head-single')
+news_date = s_soup.find(class_='date-time')
+news_pic = f"https://tengrinews.kz/{s_soup.find('picture', class_='content_main_thumb_img').find('img').get('src')}"
+news_text = s_soup.find(class_='content_main_text')
 
-    response = requests.get(url=f'https://tengrinews.kz/news/page/{page}/', headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    articles_urls = soup.find_all(class_='content_main_item_title')
-    for au in articles_urls:
-        art_url = au.find('a').get('href')
-        articles_urls_list.append(f'https://tengrinews.kz{art_url}')
-
-    time.sleep(randrange(2, 4))
-
-    with open('articles_urls_list.txt', 'w') as file:
-        for url in articles_urls_list:
-            file.write(f'{url}\n')
-
-print('Операция записи ссылок в файл завершена!')
-
-
+print(news_title.text.strip())
+print(news_date.text.strip())
+print(news_pic)
+print(news_text.text)
+# head-single
