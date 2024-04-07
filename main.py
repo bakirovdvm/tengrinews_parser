a9457e86761247ff5f70646877409f03433d66ea
Sergali Bakirov <bakirovdvm@gmail.com>
2024-04-07 23:42:39 +0600

    записываю все новости в json

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ soup = BeautifulSoup(page.text, "html.parser")
 '''
 Достаем количество страниц пагинации
 '''
-
-# pagination_count = int(soup.find('ul', class_='pagination').find_all('a')[-2].text)
-# print(pagination_count)
-
+#
+# # pagination_count = int(soup.find('ul', class_='pagination').find_all('a')[-2].text)
+# # print(pagination_count)
+#
 #
 # articles_urls_list = list()
-#
+# count = 0
 # pagination_count = 10
 # for page in range(1, pagination_count+1):
-#
+#     count += 1
 #     response = requests.get(url=f'https://tengrinews.kz/news/page/{page}/', headers=headers)
 #     soup = BeautifulSoup(response.text, "html.parser")
 #
@@ -45,24 +45,50 @@ soup = BeautifulSoup(page.text, "html.parser")
 #     with open('articles_urls_list.txt', 'w') as file:
 #         for url in articles_urls_list:
 #             file.write(f'{url}\n')
+#     print(f'Программа работает {count}/{pagination_count}')
 #
 # print(f'Операция записи ссылок в файл завершена!\n'
 #       f'Проанализировано {pagination_count} страниц')
 
-s_url = 'https://tengrinews.kz/kazakhstan_news/tokaev-pavodkah-vozmojno-samoe-krupnoe-bedstvie-poslednie-80-531444/'
-s_page = requests.get(s_url)
-s_soup = BeautifulSoup(s_page.text, "html.parser")
-
-news_title = s_soup.find(class_='head-single')
-news_date = s_soup.find(class_='date-time')
-news_pic = f"https://tengrinews.kz/{s_soup.find('picture', class_='content_main_thumb_img').find('img').get('src')}"
-news_text = s_soup.find(class_='content_main_text')
-
-print(news_title.text.strip())
-print(news_date.text.strip())
-print(news_pic)
-print(news_text.text)
-# head-single
 
 
+result_data = list()
 
+file_name = 'articles_urls_list.txt'
+print(os.path.exists(file_name))
+with open(file_name, 'r') as file:
+    # print(file.read())
+    data_urls = [line.strip() for line in file.readlines()]
+
+for s_url in data_urls[:30]:
+    s_page = requests.get(s_url)
+    s_soup = BeautifulSoup(s_page.text, "html.parser")
+
+    try:
+        news_title = s_soup.find(class_='head-single').text.strip()
+        news_date = s_soup.find(class_='date-time').text.strip()
+        news_pic = f"https://tengrinews.kz/{s_soup.find('picture', class_='content_main_thumb_img').find('img').get('src')}"
+        news_text = s_soup.find(class_='content_main_text').text.replace('\n', '')
+
+        result_data.append(
+            {
+                'url': s_url,
+                'news_title': news_title,
+                'news_date': news_date,
+                'news_pic': news_pic,
+                'news_text': news_text
+            }
+        )
+
+    except:
+        print('Ошибка...')
+    # print(news_title.text.strip())
+    # print(news_date.text.strip())
+    # print(news_pic)
+    # print(news_text.text.replace('\n', ''))
+
+with open('result_json.json', 'w') as file:
+    json.dump(result_data, file, ensure_ascii=False, indent=2)
+
+
+print('Программа завершила работу.')
